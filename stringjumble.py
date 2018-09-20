e791137c1df2f812fff268f540f970428e2a0dff
JacksonTolliday <42842756+JacksonTolliday@users.noreply.github.com>
2018-09-20 11:55:33 -0400

    Updated from Brython Server: 9/20/2018 11:55:32 AM

--- stringjumble.py
+++ stringjumble.py
@@ -1,7 +1,7 @@
 """
 stringjumble.py
 Author: <your name>
-Credit: <sources>
+Credit: https://stackoverflow.com/questions/931092/reverse-a-string-in-python
 
 Assignment:
 
@@ -25,3 +25,14 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+
+inp = input(str('Please enter a string of text (the bigger the better): '))
+print('You entered "{0}". Now jumble it:'.format(inp))
+
+revboth = inp [::-1]
+print(revboth)
+
+revwrds = inp.split(' ')
+print(revwrds)
+'''revlets
+revwrds'''
